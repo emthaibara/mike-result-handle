196170d5dae076dbb29106b705c143f94444cf08
emilia-desktop-computer <893295815@qq.com>
2025-09-26 09:40:15 +0800

    update

--- src/gen_dataset.py
+++ src/gen_dataset.py
@@ -62,8 +62,12 @@ def gen(
     master_df = master_df.merge(df_z, left_index=True, right_index=True, how='outer')
 
     output_df = pd.DataFrame(index=master_df.index, columns=all_columns)
-
-    """ qx_origin代表原始qx流量 """
+    """
+    Q1输入输出均为正数，保留1位小数；
+    Q2输入输出统一，抽水为负，发电为正，保留1位小数，不抽不发设为0；
+    Q3输入输出均为负数，保留1位小数。
+    沿程水位输出保留3位小数。
+    """
     q1_origin = cases_dict[case_id]['q1-flow_rate']
     q2_origin = cases_dict[case_id]['q2-flow_rate']
     q3_origin = cases_dict[case_id]['q3-flow_rate']
@@ -71,21 +75,22 @@ def gen(
     output_df['q2_origin'] = q2_origin
     output_df['q3_origin'] = q3_origin
 
-    output_df['q1'] = master_df[df_lhk.columns[0]].round(1)
-    if q2_origin == 0 :
-        output_df['q2'] = 0
-    elif q2_origin < 0 :
-        output_df['q2'] = -master_df[df_lhkhx.columns[0]].round(1)
-    elif q2_origin > 0 :
-        output_df['q2'] = master_df[df_lhkhx.columns[0]].round(1)
+    output_df['q1'] = master_df[df_lhk.columns[0]].abs().round(1)
 
-    if q3_origin < 0 :
-        output_df['q3'] = -master_df[df_ygyj.columns[0]].round(1)
-    elif q3_origin > 0 :
-        output_df['q3'] = master_df[df_ygyj.columns[0]].round(1)
+    if q2_origin == 0:
+        output_df['q2'] = 0.0
+    else:
+        q2_sim = master_df[df_lhkhx.columns[0]].abs().round(1)
+        if q2_origin < 0:
+            output_df['q2'] = -q2_sim
+        elif q2_origin > 0:
+            output_df['q2'] = q2_sim
+
+    output_df['q3'] = -master_df[df_ygyj.columns[0]].abs().round(1)
 
     for section_name in cross_sections:
-        output_df[section_name] = master_df[section_name]
+        output_df[section_name] = master_df[section_name].round(3)
+
     output_df.to_csv(path.join(dataset_location, f'{case_id}.csv'), index=True, index_label='time', encoding='utf-8')
 
 def move_only_csv_files(source_folder, destination_folder):
@@ -125,7 +130,7 @@ def clip(start_id : int,
         df_filtered = df[df['time'] != '2023-01-01 09:00:00']
         df_filtered.to_csv(path.join(location, f'{cid}.csv'), index=False)
 
-cases_location = r'C:\Users\lemt\PycharmProjects\mike-result-handle\assets\cases.json'
+cases_location = r'C:\Users\Administrator\Desktop\mike-result-handle\assets\cases.json'
 cases_dict = dict()
 def batch_gen_dataset(start_id : int,
                       end_id : int,
@@ -141,27 +146,53 @@ def init():
     global cases_dict
     cases_dict = {item['case_id']: item for item in cases_list}
 
-if __name__ == '__main__':
-    __base_location = r'E:\mike-simulation-result-set\pump-0-4031'
-    __dataset_location = r'C:\Users\lemt\PycharmProjects\mike-result-handle\assets\dataset\batch-one\pump-0-4031'
-    init()
-    batch_gen_dataset(0,
-                      4031,
-                      __base_location,
-                      __dataset_location)
-    clip(0,
-         4031,
-         __dataset_location)
 
-    # __base_location = r'Y:\mike-simulation-result-set\do_nothing-8064-11199'
-    # __dataset_location = r'C:\Users\lemt\PycharmProjects\mike-result-handle\assets\dataset\batch-one\do_nothing-8064-11199'
+def section_matrix():
+    section_data = {
+        'YG61-0.9HX': 1.9, 'YG61-0.75': 2.0, 'YG61-0.5': 2.1, 'YG61-0.25': 2.2, 'YG61': 2.3,
+        'YG60-1.75': 2.5, 'YG60-1.5': 2.7, 'YG60-1.25': 2.9, 'YG60-1': 3.0, 'YG60-0.5': 3.3,
+        'YG60': 3.5, 'YG59-0.5': 3.7, 'YG59-1': 3.9, 'YG59-2': 4.2, 'YG59': 4.6,
+        'YG58-1': 5.1, 'YG58': 5.5, 'YG57-1': 5.9, 'YG57-2': 6.4, 'YG57': 6.8,
+        'YG56-1': 7.3, 'YG56': 7.8, 'YG55-1': 8.2, 'YG55': 8.6, 'YG54-1': 9.2,
+        'YG54': 9.9, 'YG53-2': 10.3, 'YG53-1': 10.7, 'YG53': 11.0, 'YG52-1': 11.6,
+        'YG52': 12.3, 'YG51-1': 12.7, 'YG51': 13.2, 'YG50-1': 13.6, 'YG50': 14.0,
+        'YG49-1': 14.4, 'YG49': 14.9, 'YG48-1': 15.4, 'YG48': 16.0, 'YG47-2': 16.3,
+        'YG47': 16.8, 'YG46-1': 17.3, 'YG46': 17.8, 'YG45-1': 18.3, 'YG45': 18.9,
+        'YG44-1': 19.3, 'YG44YGJ': 19.5
+    }
+    sections = list(section_data.keys())
+    distances_km = np.array(list(section_data.values()))
+    distance_matrix_km = np.abs(distances_km[:, np.newaxis] - distances_km)
+    df_km = pd.DataFrame(distance_matrix_km, index=sections, columns=sections)
+    csv_file_km = r'C:\Users\Administrator\Desktop\mike-result-handle\assets\dataset\inter_section_distance_km.csv'
+    df_km.to_csv(csv_file_km, float_format='%.2f', encoding='utf-8')
+
+    distance_matrix_m = distance_matrix_km * 1000
+    df_m = pd.DataFrame(distance_matrix_m, index=sections, columns=sections)
+    csv_file_m = r'C:\Users\Administrator\Desktop\mike-result-handle\assets\dataset\inter_section_distance_m.csv'
+    df_m.to_csv(csv_file_m, float_format='%.0f', encoding='utf-8')
+
+if __name__ == '__main__':
+    section_matrix()
+    # __base_location = r'F:\mike-simulation-result-set\pump-0-4031'
+    # __dataset_location = r'C:\Users\Administrator\Desktop\mike-result-handle\assets\dataset\batch-one'
+    # init()
+    #
+    # batch_gen_dataset(0,
+    #                   4031,
+    #                   __base_location,
+    #                   __dataset_location)
+    # clip(0,
+    #      4031,
+    #      __dataset_location)
+    #
+    # __base_location = r'F:\mike-simulation-result-set\do_nothing-8064-11199'
     # batch_gen_dataset(8064,
     #                   11199,
     #                   __base_location,
     #                   __dataset_location)
     #
-    # __base_location = r'Y:\mike-simulation-result-set\gen-11200-15199'
-    # __dataset_location = r'C:\Users\lemt\PycharmProjects\mike-result-handle\assets\dataset\batch-one\gen-11200-15199'
+    # __base_location = r'F:\mike-simulation-result-set\gen-11200-15199'
     # batch_gen_dataset(11200,
     #                   15199,
     #                   __base_location,
